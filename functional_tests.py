80219148c27eab51a270794218e9aad07c3931d6
Panachai Vankwan <katooo241@gmail.com>
2020-02-26 23:47:34 +0700

    Creat user story and create functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,96 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_calculate(self):
+        # เขาได้ยินมาว่ามีเว็บสำหรับคิดเลข
+        # จึงเข้าเว็บไปที่หน้า Homepage
+
+        self.browser.get('http://localhost:8000')
+
+        # เขาสังเกตุว่าชื่อเว็บจะมีคำว่า calculator
+        self.assertIn('', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Calculator', header_text)
+
+        # เขาเห็นช่องสำหรับใส่ค่าเข้าไป
+        first_box = self.browser.find_element_by_tag_name('first_box').text
+        self.assertIn('Enter a 1st number:', first_box)
+        second_box = self.browser.find_element_by_tag_name('second_box').text
+        self.assertIn('Enter a 2nd number:', second_box)
+
+        # เขาต้องการบวกเลข
+        # เขาใส่เลขไปในช่องที่ 1
+        first_textbox = self.browser.find_element_by_id("first_textbox")
+        first_textbox.send_keys('2')
+        # เขาใส่เลขไปในช่องที่ 2
+        second_textbox = self.browser.find_element_by_id("second_textbox")
+        second_textbox.send_keys('3')
+        # เข้ากุปปุ่ม add
+        add_button = self.browser.find_element_by_id("add")
+        add_button.click()
+        # เขาเห็นผลลัพธ์มีค่าเท่ากับ 5
+        result_text = self.browser.find_element_by_tag_name('result').text
+        self.assertIn('Your Result : 5.0', result_text)
+
+
+        # เขาต้องการลบเลข
+        # เขาใส่เลขไปในช่องที่ 1
+        first_textbox = self.browser.find_element_by_id("first_textbox")
+        first_textbox.send_keys('199')
+        # เขาใส่เลขไปในช่องที่ 2
+        second_textbox = self.browser.find_element_by_id("second_textbox")
+        second_textbox.send_keys('198')
+        # เข้ากุปปุ่ม add
+        add_button = self.browser.find_element_by_id("sub")
+        add_button.click()
+        # เขาเห็นผลลัพธ์มีค่าเท่ากับ 5
+        result_text = self.browser.find_element_by_tag_name('result').text
+        self.assertIn('Your Result : 1.0', result_text)
+
+
+        # เขาต้องการคูณเลข
+        # เขาใส่เลขไปในช่องที่ 1
+        first_textbox = self.browser.find_element_by_id("first_textbox")
+        first_textbox.send_keys('5')
+        # เขาใส่เลขไปในช่องที่ 2
+        second_textbox = self.browser.find_element_by_id("second_textbox")
+        second_textbox.send_keys('5')
+        # เข้ากุปปุ่ม add
+        add_button = self.browser.find_element_by_id("mul")
+        add_button.click()
+        time.sleep(3)
+        # เขาเห็นผลลัพธ์มีค่าเท่ากับ 5
+        result_text = self.browser.find_element_by_tag_name('result').text
+        self.assertIn('Your Result : 25.0', result_text)
+        time.sleep(3)
+
+        # เขาต้องการหารเลข
+        # เขาใส่เลขไปในช่องที่ 1
+        first_textbox = self.browser.find_element_by_id("first_textbox")
+        first_textbox.send_keys('100')
+        # เขาใส่เลขไปในช่องที่ 2
+        second_textbox = self.browser.find_element_by_id("second_textbox")
+        second_textbox.send_keys('5')
+        # เข้ากุปปุ่ม add
+        add_button = self.browser.find_element_by_id("div")
+        add_button.click()
+        time.sleep(3)
+        # เขาเห็นผลลัพธ์มีค่าเท่ากับ 5
+        result_text = self.browser.find_element_by_tag_name('result').text
+        self.assertIn('Your Result : 20.0', result_text)
+        time.sleep(3)
+
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
