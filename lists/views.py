f6c583d9f8b23f31bf15bee1c63459e00a6f320e
Panachai Vankwan <katooo241@gmail.com>
2020-02-26 22:30:54 +0700

    Create operate function

--- lists/views.py
+++ lists/views.py
@@ -6,11 +6,20 @@ from django.http import HttpResponse, Http404,HttpResponseRedirect,HttpResponseN
 def home_page(request):
     return render(request, 'home.html')
 
-def add(request):
+def operate(request):
     res = 0
     if request.method == 'POST':
-        val1 = int(request.POST['num1'])
-        val2 = int(request.POST['num2'])
-
-        res = val1 + val2
-    return render(request,'home.html',{'result': res})
+        val1 = float(request.POST['num1'])
+        val2 = float(request.POST['num2'])
+        # if 'add' in request.POST:
+            # res = val1 + val2
+            # return render(request,'home.html',{'result': res})
+        # elif 'sub' in request.POST:
+        #     res = val1 - val2
+        #     return render(request,'home.html',{'result': res})
+        # elif 'mul' in request.POST:
+        #     res = val1 * val2
+        #     return render(request,'home.html',{'result': res})
+        # elif 'div' in request.POST:
+        #     res = val1 / val2
+        #     return render(request,'home.html',{'result': res})
